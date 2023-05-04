7e4e2741741ac4a9e4f737655a61fd7388f76873
subtleparesh <subtleparesh@gmail.com>
2023-05-04 08:45:02 +0530

    Update dbt flow  commands

--- workflows/dbt_flow.py
+++ workflows/dbt_flow.py
@@ -4,7 +4,7 @@ from prefect_dbt.cli.commands import DbtCoreOperation
 @flow
 def trigger_dbt_flow():
     result = DbtCoreOperation(
-        commands=["pwd", "dbt debug", "dbt run"],
+        commands=["ls", "dbt debug", "dbt run"],
         project_dir="./dbt_transformations",
         profiles_dir="./dbt_transformations"
     ).run()
