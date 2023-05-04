6355393e919a91d0abd3aa0eb925ee765872771c
subtleparesh <subtleparesh@gmail.com>
2023-05-04 17:51:22 +0530

    Enable streaming output in dbt

--- workflows/dbt_flow.py
+++ workflows/dbt_flow.py
@@ -5,6 +5,7 @@ from prefect_dbt.cli.commands import DbtCoreOperation
 def trigger_dbt_flow():
     result = DbtCoreOperation(
         commands=["ls &&", "dbt debug", "dbt run"],
-        project_dir="./dbt_transformations",
-        profiles_dir="./dbt_transformations"
+        project_dir="dbt_transformations",
+        profiles_dir="dbt_transformations",
+        stream_output=True
     ).run()
